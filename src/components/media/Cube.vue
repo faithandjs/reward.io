4b58c8ec63c32045a36d4c65fae4703f1a7dac5d
faithandjs <faiththe1rst@gmail.com>
2023-05-07 12:04:55 +0100

    padding to the cube

--- src/components/media/Cube.vue
+++ src/components/media/Cube.vue
@@ -3,3 +3,15 @@
     <img src="../../assets/vid.gif" alt="">
   </div>
 </template>
+
+<style scoped>
+img {
+  margin-left: 100px;
+}
+
+@media screen and (max-width: 1024px) {
+  img {
+    margin-left: 0;
+  }
+}
+</style>
